9fe959e80cac30e98cc0313e400caf793863bd2f
Alex Dellinger <alexdellinger11@gmail.com>
2015-08-27 17:08:54 -0400

    save

---
+++ story/intro.py
@@ -0,0 +1,24 @@
+"""A Time Paradox
+
+    Over the ages of time there have been many great events that have changed our world. These acomplishments have
+plunged the human race further and further into advancements in technology, discovery, and knowledge. But humans 
+can be selfish. Men and Women in the future have use of time machines and use them to create paradox's. These 
+paradox's are making the human race less advanced in technology, speech, discovery, and knowledge. some people believe
+the world is flat. Others think it is eight times the size it really is. And some people think it is owned by the roman
+and greek gods. 
+    
+    You however, are unaffected by this. You had started to forget, but you are a highly inteligent person, 
+who knows how dangerous paradox's can be. You have written a document telling you what the world is like today.
+you now remember what the world is like. You know that this change in humans is caused by silly choices in the 
+future and you must go and undo them. You must build a time machine and go through the ages undoing all that has been
+done. 
+
+    You at least know three of the times that the people came: The Medevial Times, Ancient Egypt, and finally, the 
+US in the 1800's. You must go through many more time periods than this however. You even might have to travel to 
+different worlds and dimensions. If you fail your mission, The human race as we know it could fade away... forever.
+If you succeed in your mission the human race would be able to evolve and be smarter than they ever were. However the 
+jealous people in the future have placed robots in the times they went to and changed to make sure no one can undo 
+them and they can remain legend when they die. This journey will be perilous, but you have a secret protecter you 
+do not know of...
+
+"""
